af330aedf64cc4fdb3622ec5f464caa683271abc
unknown <nguy2935@umn.edu>
2021-11-14 12:37:22 -0600

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+let currentDate = new Date();
+let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+console.log(time);
+
+let currentDay = currentDate.getDate(); + "," + currentDay.getMonth();
