05566d6f2399e5f4280bd418af21cea70e221069
unknown <Sophia Nguyen>
2021-11-29 14:26:31 -0600

    added a function that color codes the workday schedule

--- Develop/script.js
+++ Develop/script.js
@@ -11,11 +11,44 @@ getTodayDate();
 
 
 // save
-function save() {
-    $(".saveBn").on("click", function() { 
-        
+$(document).ready(function(){
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
     })
 
+})
+
+
+function trackTime() {
+    var currentTime = moment().hour();
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    // this checks the time and determines whether it's in the past, present, or future. Will add background color indicators
+    if (blockTime < currentTime ) {
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    $(this).addClass("past");
+    }
+
+    else if (blockTime === currentTime ) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+
+    }
+})
 }
+
+trackTime();
 
 // json 
